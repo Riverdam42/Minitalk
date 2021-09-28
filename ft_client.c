b2c413619be3a4af6e9f0e5bff91a378a560b219
Kosuke Kawano <kkawano@c1r25s1.42tokyo.jp>
2021-09-29 01:23:04 +0900

    25%

--- ft_client.c
+++ ft_client.c
@@ -6,43 +6,59 @@
 /*   By: kkawano <kkawano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/23 20:00:44 by kkawano           #+#    #+#             */
-/*   Updated: 2021/09/27 14:07:36 by kkawano          ###   ########.fr       */
+/*   Updated: 2021/09/28 14:03:09 by kkawano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-pid_t   analyze_pid(char *str)
+static void	signal_handler(int signal)
 {
-    int n;
-
-    n = 0;
-    while (*str)
-    {
-        if ('0' <= *str && *str <= '9')
-            n = n * 10 + *str - '0';
-        else
-            return (-1);
-        str++;
-    }
-    return (n);
+	if (signal == SIGUSR1)
+		flag = 1;
 }
 
-static void	send_char(pid_t pid, char c)
+void	send_str(char *str, int pid)
 {
-	int				bit;
-	int				i;
-	unsigned char	uc;
+	int32_t	bit;
+	int		i;
 
-	uc = (unsigned char)c;
-	i = 0;
-	while (i < 8)
+	while (*str)
 	{
-		usleep(50);
-		bit = (uc >> i) & 0x01;
-		if (kill(pid, SIGUSR1 + bit) == -1)
-			ft_error_exit("kill error");
-		i++;
+		bit = (unsigned char)*str++;
+		i = -1;
+		while (++i < 32)
+		{
+			if ((bit & 1) == 0)
+			{
+				if (kill(pid, SIGUSR1) == -1)
+					exit(print_error("KILL_ERROR"));
+			}
+			else
+			{
+				if (kill(pid, SIGUSR2) == -1)
+					exit(print_error("KILL_ERROR"));
+			}
+			bit = bit >> 1;
+			while (flag == 0)
+				;
+			flag = 0;
+		}
 	}
 }
 
+int	main(int argc, char **argv)
+{
+	int				pid_server;
+	struct sigaction	act;
+
+	act.sa_handler = &signal_handler;
+	if (sigaction(SIGUSR1, &act, NULL) == -1)
+		exit(print_error("SIGNAL_ERROR"));
+	if (argc != 3)
+		return (print_error("INVALID_ARGUMENT"));
+	pid_server = ft_atoi(argv[1]);
+	if (pid_server == 0 || pid_server == -1 || kill(pid_server, 0) == -1)
+		return (print_error("EXIT_FAILURE"));
+	send_str(argv[2], pid_server);
+}
--- ft_server.c
+++ ft_server.c
@@ -6,41 +6,53 @@
 /*   By: kkawano <kkawano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/23 20:00:59 by kkawano           #+#    #+#             */
-/*   Updated: 2021/09/27 19:55:47 by kkawano          ###   ########.fr       */
+/*   Updated: 2021/09/28 20:50:04 by kkawano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-#include <stdio.h>
 
-static void	handle_signal(int signal)
+static int	end_of_transmission(unsigned char *buf, int index, siginfo_t *info)
 {
-	g_receive_signal = signal;
+	ft_putendl_fd((char *)buf, 1);
+	ft_bzero(buf, index);
+	kill(info->si_pid, SIGUSR2);
+	return (0);
 }
 
-void	set_signal(void)
+static void	signal_handler(int sig, siginfo_t *info, void *ucontext)
 {
-	signal(SIGUSR1, &handle_signal);
-}
+	static unsigned char	buf[33];
+	static int				index;
+	static int				bit;
+	static unsigned char	uc;
 
-void	set_signal(void)
-{
-	struct sigaction	sa;
-
-	sa.sa_handler = &handle_signal;
-	sigemptyset(&sa.sa_mask);
-	sigaddset(&sa_sigusr1.sa_mask, SIGUSR1);
-	sigaction(SIGUSR1, &sa, NULL);
+	(void)ucontext;
+	if (sig == SIGUSR2)
+		uc |= (1 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		if (index > 33 - 1)
+			print_error("BUF_OVERFLOW");
+		if (uc == 0)
+			index = end_of_transmission(buf, index, info);
+		else
+			buf[index++] = uc;
+		bit = 0;
+		uc = 0;
+	}
 }
 
 int	main(void)
 {
-	pid_t       pid_client;
+	int       pid_client;
+	struct sigaction act;
 
     pid_client = getpid();
     ft_putnbr_fd(pid_client, 1);
     write(1, "\n", 1);
-	//receiver(signal_server_handler); //handlerを確認する
+	act.sa_handler = &signal_handler; //handlerを確認する
     while (1)
 		;
 	return (0);
